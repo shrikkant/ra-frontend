236a117da92e10cfe83c9b9bcac8479d4dd4e3e5
Shri Shinde <shrikkant@gmail.com>
2023-07-01 18:12:32 +0530

    fix

--- config/constants.ts
+++ config/constants.ts
@@ -1,5 +1,9 @@
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig: envConfig } = getConfig()
+
 export const ENV = process.env.REACT_APP_ENV!;
-export const BASE_API_URL = "https://labs.rentacross.com/api/" // process.env.REACT_APP_API_URL!;
+export const BASE_API_URL = envConfig.BASE_API_URL;
 export const REACT_APP_URL = process.env.REACT_APP_URL!;
 export const TOKEN_COOKIE_KEY = 'access_token'
 export const TOKEN_HEADER_KEY = 'authorization'
