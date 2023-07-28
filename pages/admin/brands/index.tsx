3800015bb00a7a26bcd26a822bfccba20be87a83
Shri Shinde <shrikkant@gmail.com>
2023-07-28 15:20:29 +0530

    fix

--- pages/admin/brands/index.tsx
+++ pages/admin/brands/index.tsx
@@ -1,10 +1,4 @@
-
-import {
-  Button,
-  Layout,
-  Space,
-  Tag,
-} from "antd";
+import { Button, Layout, Space, Tag } from "antd";
 import Table from "antd/lib/table/Table";
 
 import styles from "styles/orders.module.css";
@@ -14,37 +8,45 @@ import AppNav from "components/AppNav";
 import { AppFooter } from "components/footer";
 import AppHeader from "../../../components/header";
 
-import { getCustomers, setCustomers, getBrands, setBrands } from "../../../app-store/admin/index.slice";
+import {
+  getCustomers,
+  setCustomers,
+  getBrands,
+  setBrands,
+} from "../../../app-store/admin/index.slice";
 import { useDispatch, useSelector } from "react-redux";
 
 import MyPageHeader from "components/MyPageHeader";
 
-import Moment from 'moment';
+import Moment from "moment";
 import React, { useEffect } from "react";
 import { fetchBrands } from "api/admin/index.api";
 import { useRouter } from "next/router";
 import Loader from "components/Loader";
 import { ArrowsPointingOutIcon } from "@heroicons/react/24/outline";
+import { AppLayout } from "../../../components/AppLayout";
 
 const columns = [
   {
-    title: 'Status',
-    dataIndex: 'id',
-    key: 'id',
-    render: (val) => (val ? <ArrowsPointingOutIcon /> : "")
+    title: "Status",
+    dataIndex: "id",
+    key: "id",
+    render: (val) => (val ? <ArrowsPointingOutIcon /> : ""),
   },
   {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: (text, record) => { return text }
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+    render: (text, record) => {
+      return text;
+    },
   },
   {
-    title: 'Descriptiopn',
-    dataIndex: 'description',
-    key: 'description',
-  }
-]
+    title: "Descriptiopn",
+    dataIndex: "description",
+    key: "description",
+  },
+];
 
 export default function Customers() {
   const router = useRouter();
@@ -52,40 +54,32 @@ export default function Customers() {
 
   const brands = useSelector(getBrands);
   const dispatch = useDispatch();
-  const df = Moment().format('DD MMM');
-
+  const df = Moment().format("DD MMM");
 
   const loadBrands = () => {
     setLoading(true);
-    router.isReady && fetchBrands().then(data => {
-      dispatch(setBrands(data));
-      setLoading(false);
-    })
-  }
+    router.isReady &&
+      fetchBrands().then((data) => {
+        dispatch(setBrands(data));
+        setLoading(false);
+      });
+  };
 
   useEffect(() => {
     loadBrands();
-  }, [router.isReady])
+  }, [router.isReady]);
 
+  return (
+    <AppLayout>
+      <MyPageHeader title={"Brands"} subtitle={""}></MyPageHeader>
 
-
-
-  return (<Content>
-    <AppHeader></AppHeader>
-    <Content className="main-content">
-      <AppNav></AppNav>
-      <Content className={styles.content}>
-
-        <MyPageHeader title={"Brands"} subtitle={""}></MyPageHeader>
-
-        <Content style={{ padding: '16px 16px' }}>
-            {loading ? <Loader/> : <Table columns={columns} dataSource={brands} rowKey={"id"} />}
-        </Content>
+      <Content style={{ padding: "16px 16px" }}>
+        {loading ? (
+          <Loader />
+        ) : (
+          <Table columns={columns} dataSource={brands} rowKey={"id"} />
+        )}
       </Content>
-    </Content>
-
-
-    <AppFooter></AppFooter>
-  </Content>)
+    </AppLayout>
+  );
 }
-
