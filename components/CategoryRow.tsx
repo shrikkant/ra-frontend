0356b82b9b50e52346b23952c020fa0334d309f0
Shri Shinde <shrikkant@gmail.com>
2023-09-19 18:30:59 +0530

    fix

--- components/CategoryRow.tsx
+++ components/CategoryRow.tsx
@@ -8,7 +8,6 @@ import React from "react";
 
 export default function CategoryRow(props: any) {
   return (
-
       <Content className="r-comp flex gap-1 sm:gap-4 flex-wrap p-2">
         {props.category.products.map((product: any) => (
           <ProductCard key={product.id} product={product} priority={true}></ProductCard>
