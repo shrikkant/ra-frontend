7ae2da31d6ad155d66d539ebcd20cd716b5bb528
Shri Shinde <shrikkant@gmail.com>
2023-09-19 18:32:56 +0530

    category row

--- components/CategoryRow.tsx
+++ components/CategoryRow.tsx
@@ -1,8 +1,5 @@
-import { Card, Space } from "antd";
-import Meta from "antd/lib/card/Meta";
+
 import { Content } from "antd/lib/layout/layout";
-import Link from "next/link";
-import styles from '../styles/Home.module.css'
 import ProductCard from "./ProductCard";
 import React from "react";
 
