3892c4eccfe0b68af60d470d6cc374b526b6ae99
Shri Shinde <shrikkant@gmail.com>
2023-09-20 14:14:13 +0530

    test

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     BASE_API_URL: process.env.REACT_APP_API_URL,
   },
   images: {
-    domains: ["rentacross.com", "www.rentacross.com"],
+    domains: ["images.pexels.com"],
   },
 }
 
