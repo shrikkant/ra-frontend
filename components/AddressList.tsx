f8e880fc72e94bef92d82f5b8bc45ca319536af8
Shri Shinde <shrikkant@gmail.com>
2023-09-28 15:21:53 +0530

    fix radio checked.

--- components/AddressList.tsx
+++ components/AddressList.tsx
@@ -16,17 +16,28 @@ export const AddressList = ({
   const [selectedAddress, setSelectedAddress] = useState(0);
 
   const checkRadio = (addressId) => {
-    document.getElementById("addr_" + addressId).checked = true;
+    const el = document.getElementById("addr_" + addressId) as HTMLInputElement;
+    el.checked = true;
     setSelectedAddress(addressId);
     onAddressChange(addressId);
   };
   return (
     <>
-      <RadioListItem value={-1} onCheck={checkRadio} key={"storePickup"} active={selectedAddress === -1} >
+      <RadioListItem
+        value={-1}
+        onCheck={checkRadio}
+        key={"storePickup"}
+        active={selectedAddress === -1}
+      >
         <span className={"font-semibold"}>Store Pickup</span>
       </RadioListItem>
       {addressList.map((addr) => (
-        <RadioListItem key={addr.id} value={addr.id} onCheck={checkRadio} active={addr.id === selectedAddress}>
+        <RadioListItem
+          key={addr.id}
+          value={addr.id}
+          onCheck={checkRadio}
+          active={addr.id === selectedAddress}
+        >
           <span className={"font-bold"}>{userName}</span>
           <span>{addr.address_line_1},</span>
           <span>{addr.address_line_2},</span>
