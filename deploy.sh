090a0e45e47546addfc2200dd14303590f28e9c1
Shri Shinde <shrikkant@gmail.com>
2024-01-12 17:06:32 +0530

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd ~/ra-frontend;
-git pull;
-docker-compose -f docker-compose.prod.yml build && docker-compose -f docker-compose.prod.yml down && docker system prune -f &&  docker-compose -f docker-compose.prod.yml up --force-recreate -d;
+git pull -4;
+docker compose -f docker-compose.prod.yml build && docker compose -f docker-compose.prod.yml down && docker system prune -f &&  docker compose -f docker-compose.prod.yml up --force-recreate -d;
