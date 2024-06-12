efb706171d8e36f60769a37cec3a126e7480f7fd
Shri Shinde <shrikkant@gmail.com>
2024-06-12 17:23:34 +0530

    fix

--- app-store/app-defaults/types.ts
+++ app-store/app-defaults/types.ts
@@ -1,6 +1,16 @@
 import { IProductCategory } from "../types";
 
+
+export interface ITestimonial {
+  id?: number;
+  name: string;
+  description: string;
+  img: string;
+  rating: number;
+}
 export interface AppDefaultsState {
   categories?: IProductCategory[];
   searchDefaults?: any;
 }
+
+
