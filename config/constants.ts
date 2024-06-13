2ba76ab3b4aad47fb48e2c74aac4349bfd94844d
Shri Shinde <shrikkant@gmail.com>
2024-06-13 23:13:19 +0530

    fix now

--- config/constants.ts
+++ config/constants.ts
@@ -5,6 +5,7 @@ import getConfig from "next/config";
 export const ENV = process.env.REACT_APP_ENV!;
 // export const BASE_API_URL = envConfig.BASE_API_URL;
 // export const BASE_URL = envConfig.BASE_URL;
+export const BASE_API_URL = process.env.REACT_APP_API_URL!;
 export const REACT_APP_URL = process.env.REACT_APP_URL!;
 export const TOKEN_COOKIE_KEY = "access_token";
 export const TOKEN_HEADER_KEY = "authorization";
