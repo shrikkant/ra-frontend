38b32924a09850c022c8bee1edc1c995aad33779
Shri Shinde <shrikkant@gmail.com>
2024-06-13 08:20:18 +0530

    parking for now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,7 @@ const MyApp: FC<AppProps> = ({ Component, ...rest }) => {
         <link rel="shortcut icon" href="/assets/img/rentacross_logo.ico" type="image/x-icon"></link>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
         <meta name="description" content="RentAcross is an online rental platform for cameras, lenses, gopros, insta360 action cameras & camping and adventure gear."></meta>
-        {/* <meta name="robots" content="noindex" /> */}
+        <meta name="robots" content="noindex" />
 
         </Head>
         <Component/>
