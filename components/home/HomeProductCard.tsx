fa169684a9549d0a3534ab65f25cf086b2b472e2
Shri Shinde <shrikkant@gmail.com>
2024-06-14 00:25:57 +0530

    lets try now

--- components/home/HomeProductCard.tsx
+++ components/home/HomeProductCard.tsx
@@ -2,7 +2,7 @@ import { IProduct } from "../../app-store/types";
 import Image from "next/image";
 import PriceTag from "../PriceTag";
 
-export default function HomeProductCard({product}: {product: IProduct}) {
+export default function HomeProductCard({product}: {product: any}) {
 
   if (!product)
     return (<div>Product not found</div>)
@@ -10,11 +10,11 @@ export default function HomeProductCard({product}: {product: IProduct}) {
   return (<div key={product.id} className="col-sm-6 col-lg-3">
     <div className="product-item">
       <span className="top-sale">top sale</span>
-      {/* <ul className="product-icon-top">
-        <li><a href="#"><i className="fa fa-refresh" aria-hidden="true"></i></a></li>
-        <li><a href="#"><i className="fa fa-heart" aria-hidden="true"></i></a></li>
-      </ul> */}
+
       <a href="single-shop.html" className="product-img">
+
+        {product.photos[0]  && <Image alt={product.title} className={"p-2 sm:p-5"} width={200} height={-1}
+          src={"https://www.rentacross.com" + product.photos[0].path}></Image>}
 
       </a>
       <div className="product-item-cover">
