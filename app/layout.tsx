0f3ed7d0565ed75e55bd3cce5681894659769221
Shri Shinde <shrikkant@gmail.com>
2024-06-14 19:34:53 +0530

    no follow

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,8 @@ export default function RootLayout({
         <meta name="keywords"
           content="Rent DSLR Cameras, Rent GoPro, Rent Video Cameras, Rent DSLR Lenses, Camera Rental Community, Online Camera Rental Store" />
 
+        <meta name="robots" content="noindex" />
+
         <link rel="stylesheet" href="/assets/v2/css/slick.min.css"></link>
         <link rel="stylesheet" href="/assets/v2/css/bootstrap-grid.css"></link>
         <link rel="stylesheet" href="/assets/v2/css/font-awesome.min.css"></link>
