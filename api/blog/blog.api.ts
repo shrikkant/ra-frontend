413e43c4033382eac4bb4b96deaf693b6d9a69ed
Shri Shinde <shrikkant@gmail.com>
2024-06-28 14:02:14 +0530

    fx

--- api/blog/blog.api.ts
+++ api/blog/blog.api.ts
@@ -1,22 +1,14 @@
 import { fetchData } from "../axios.config";
 
 export async function fetchBlogs(page: number, limit?: number, type?: number,): Promise<any> {
-  try {
-    // const response = await httpClient.get<any>(`blog?limit=4&page=${page || 1}`);
-    const res = await fetchData(`blog?limit=${limit ? limit : 4}&page=1${type ? `&type=${type}` : ''}`);
+  // const response = await httpClient.get<any>(`blog?limit=4&page=${page || 1}`);
+  const res = await fetchData(`blog?limit=${limit ? limit : 4}&page=1${type ? `&type=${type}` : ''}`);
 
-    return res;
+  return res;
 
-  } catch (e) {
-    throw e;
-  }
 }
 
 export async function fetchBlogBySlug(slug: string): Promise<any> {
-  try {
-    const res = await fetchData(`blog/.by.slug/${slug}`);
-    return res;
-  } catch (e) {
-    throw e;
-  }
+  const res = await fetchData(`blog/.by.slug/${slug}`);
+  return res;
 }
