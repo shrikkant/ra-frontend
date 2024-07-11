e44149cb923e25219c39505d437d8637b173d246
Shri Shinde <shrikkant@gmail.com>
2024-07-11 17:29:37 +0530

    fix

---
+++ app/StoreProvider.tsx
@@ -0,0 +1,25 @@
+'use client'
+import React from 'react'
+import { useRef } from 'react'
+import { Provider } from 'react-redux'
+import { makeStore, AppStore } from '../app-store/store'
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+export default function StoreProvider({ children }: {
+  children: React.ReactNode
+}) {
+  const storeRef = useRef<AppStore>()
+  if (!storeRef.current) {
+    storeRef.current = makeStore()
+  }
+  const persistor = persistStore(storeRef.current);
+
+  return (
+    <Provider store={storeRef.current}>
+      <PersistGate loading={false} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
+}
