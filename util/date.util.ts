b6b20d45b71ec091bb277b3fe24a6bae5505aa86
Shri Shinde <shrikkant@gmail.com>
2024-07-17 19:17:00 +0530

    fix

--- util/date.util.ts
+++ util/date.util.ts
@@ -22,7 +22,7 @@ const rangeDisplay = function (range: IDateRange) {
 const dateDisplay = function (date: Date | undefined) {
   if (!date)
     return "Invalid Date";
-  return Moment(new Date(date)).utcOffset(0).format("D MMM YYYY");
+  return Moment(new Date(date)).format("D MMM YYYY");
 }
 
 const timeAgo = (timestamp) => {
