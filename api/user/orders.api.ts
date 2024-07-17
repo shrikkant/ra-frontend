4010885c8019bfeb74c078af7a88e17d104b3e02
Shri Shinde <shrikkant@gmail.com>
2024-07-17 14:50:26 +0530

    fix dates

--- api/user/orders.api.ts
+++ api/user/orders.api.ts
@@ -30,8 +30,8 @@ export const addToCart = async (
 
   await httpClient.post(`/user/carts`, {
     date: {
-      startDate: startDate,
-      endDate: endDate,
+      startDate: new Date(startDate + "+0:00"),
+      endDate: new Date(endDate + "+0:00"),
       startTime: 9,
       endTime: 9,
       rentalDays: 1,
