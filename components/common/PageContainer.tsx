fa6b6df49c73a1453840600e086088fb9fa336d8
Shri Shinde <shrikkant@gmail.com>
2024-08-24 12:52:44 +0530

    fix height issue

--- components/common/PageContainer.tsx
+++ components/common/PageContainer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function PageContainer({ children }) {
   return (
-    <div className="container mx-auto h-screen">
+    <div className="container mx-auto">
       {children}
     </div>
   )
