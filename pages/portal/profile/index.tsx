54f44c268d05fd21a6e57f8202ca9f3c7acabe2c
Shri Shinde <shrikkant@gmail.com>
2024-08-24 18:54:03 +0530

    fix

--- pages/portal/profile/index.tsx
+++ pages/portal/profile/index.tsx
@@ -4,6 +4,8 @@ import { useSelector } from "react-redux";
 import { selectAuthState } from "app-store/auth/auth.slice";
 import { AppLayout } from "components/AppLayout";
 import MyPageHeader from "components/MyPageHeader";
+import { PhoneIcon, UserPlusIcon } from "@heroicons/react/24/outline";
+import { FaAddressBook, FaPhone } from "react-icons/fa";
 
 export default function MyProfile() {
 
@@ -19,39 +21,32 @@ export default function MyProfile() {
               <div className="items-center sm:flex xl:block 2xl:flex sm:space-x-4 xl:space-x-0 2xl:space-x-4">
                 <img
                   className="mb-4 rounded-lg w-28 h-28 sm:mb-0 xl:mb-4 2xl:mb-0"
-                  src="https://flowbite-admin-dashboard.vercel.app/images/users/bonnie-green-2x.png"
+                  src={loggedUser.profile_pic}
                   alt="Jese picture"
                 />
                 <div>
                   <h3 className="mb-1 text-xl font-bold text-gray-900 dark:text-white">
                     {loggedUser.firstname + " " + loggedUser.lastname}
                   </h3>
-                  <div className="mb-4 text-sm text-gray-500 dark:text-gray-400">
-                    JPG, GIF or PNG. Max size of 800K
+                  <div className="mb-4 text-sm text-gray-500 dark:text-gray-400 flex items-center gap-x-1">
+                    <div>
+                      <FaAddressBook />
+                    </div>
+                    <div>
+                      {loggedUser.email_address}
+                    </div>
+
                   </div>
-                  <div className="flex items-center space-x-4">
-                    <button
-                      type="button"
-                      className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                    >
-                      <svg
-                        className="w-4 h-4 mr-2 -ml-1"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M5.5 13a3.5 3.5 0 01-.369-6.98 4 4 0 117.753-1.977A4.5 4.5 0 1113.5 13H11V9.413l1.293 1.293a1 1 0 001.414-1.414l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13H5.5z"></path>
-                        <path d="M9 13h2v5a1 1 0 11-2 0v-5z"></path>
-                      </svg>
-                      Upload picture
-                    </button>
-                    <button
-                      type="button"
-                      className="py-2 px-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                    >
-                      Delete
-                    </button>
+                  <div className="mb-4 text-sm text-gray-500 dark:text-gray-400 flex items-center gap-x-1">
+                    <div>
+                      <FaPhone />
+                    </div>
+                    <div>
+                      {loggedUser.phone}
+                    </div>
+
                   </div>
+
                 </div>
               </div>
             </div>
