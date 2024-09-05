c06506d1cf537e62141027fea664858cd9228b11
Shri Shinde <shrikkant@gmail.com>
2024-09-05 14:26:38 +0530

    fix

--- pages/portal/admin/customers/index.tsx
+++ pages/portal/admin/customers/index.tsx
@@ -30,6 +30,10 @@ export default function Customers() {
     });
   };
 
+  const visitProfile = (id) => {
+    router.push("/portal/admin/customers/" + id);
+  }
+
   useEffect(() => {
     router.isReady && loadCustomers();
   }, [router.isReady]);
@@ -50,21 +54,26 @@ export default function Customers() {
                   className="flex justify-between gap-x-6 py-5 items-center"
                 >
                   <div className="flex gap-x-4 xs:w-full sm:w-72">
-                    <img
-                      className="h-12 w-12 flex-none rounded-full bg-gray-50"
-                      src={person.profile_pic}
-                      alt=""
-                    />
                     <div className="min-w-0 flex-auto xs:w-full">
+
                       <div className="flex items-center w-full justify-between">
-                        <div className="w-56">
-                          <div className="text-sm font-semibold leading-6 text-gray-900">
-                            {person.firstname + " " + person.lastname}
-                          </div>
-                          <div className="mt-1 truncate text-xs leading-5 text-gray-500">
-                            {person.email_address}
+                        <div onClick={() => visitProfile(person.id)} className="cursor-pointer flex gap-x-2">
+                          <img
+                            className="h-12 w-12 flex-none rounded-full bg-gray-50"
+                            src={person.profile_pic}
+                            alt=""
+                          />
+                          <div className="w-56">
+                            <div className="text-sm font-semibold leading-6 text-gray-900">
+                              {person.firstname + " " + person.lastname}
+                            </div>
+                            <div className="mt-1 truncate text-xs leading-5 text-gray-500">
+                              {person.email_address}
+                            </div>
                           </div>
                         </div>
+
+
                         <div className="align-center">
                           <Link target="_blank" href={`https://wa.me/91${person.phone}?text=Hi ${person.firstname}, Thank you for joining RentAcross. What are you looking to rent today?`}>
                             <FaWhatsappSquare size={"36"} />
