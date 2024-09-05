1417443f64fa2487d66e1e89adcb720f5e5f21a9
Shri Shinde <shrikkant@gmail.com>
2024-09-05 13:29:08 +0530

    need the whatsapp shit

--- pages/portal/admin/customers/index.tsx
+++ pages/portal/admin/customers/index.tsx
@@ -13,6 +13,8 @@ import Loader from "components/Loader";
 import { Time } from "components/Time";
 import { PhoneIcon } from "@heroicons/react/24/outline";
 import { AppLayout } from "components/AppLayout";
+import { FaWhatsappSquare } from "react-icons/fa";
+import Link from "next/link";
 
 export default function Customers() {
   const router = useRouter();
@@ -45,26 +47,37 @@ export default function Customers() {
               customers.map((person) => (
                 <li
                   key={person.email_address}
-                  className="flex justify-between gap-x-6 py-5"
+                  className="flex justify-between gap-x-6 py-5 items-center"
                 >
-                  <div className="flex gap-x-4">
+                  <div className="flex gap-x-4 xs:w-full sm:w-72">
                     <img
                       className="h-12 w-12 flex-none rounded-full bg-gray-50"
                       src={person.profile_pic}
                       alt=""
                     />
-                    <div className="min-w-0 flex-auto">
-                      <div className="text-sm font-semibold leading-6 text-gray-900">
-                        {person.firstname + " " + person.lastname}
+                    <div className="min-w-0 flex-auto xs:w-full">
+                      <div className="flex items-center w-full justify-between">
+                        <div className="w-56">
+                          <div className="text-sm font-semibold leading-6 text-gray-900">
+                            {person.firstname + " " + person.lastname}
+                          </div>
+                          <div className="mt-1 truncate text-xs leading-5 text-gray-500">
+                            {person.email_address}
+                          </div>
+                        </div>
+                        <div className="align-center">
+                          <Link target="_blank" href={`https://wa.me/91${person.phone}?text=Hi ${person.firstname}, Thank you for joining RentAcross. What are you looking to rent today?`}>
+                            <FaWhatsappSquare size={"36"} />
+                          </Link>
+                        </div>
                       </div>
-                      <div className="mt-1 truncate text-xs leading-5 text-gray-500">
-                        {person.email_address}
-                      </div>
+
                     </div>
                   </div>
+
                   <div className="hidden sm:flex sm:flex-col sm:items-end">
-                    <div className="text-md leading-6 text-gray-900 flex">
-                      <PhoneIcon style={{ width: "1.1rem" }}></PhoneIcon>
+                    <div className="text-md leading-6 text-gray-900 flex gap-x-1">
+                      <PhoneIcon style={{ width: "1.1rem" }} />
                       <div>{person.phone}</div>
                     </div>
 
@@ -72,6 +85,7 @@ export default function Customers() {
                       <Time time={person.created_ts} />
                     </div>
                   </div>
+
                 </li>
               ))}
           </ul>
