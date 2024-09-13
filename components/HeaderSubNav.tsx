5e9f2533281a591ae83fae0533e6b8ce2804bbf3
Shri Shinde <shrikkant@gmail.com>
2024-09-13 19:16:02 +0530

    remove some shit

--- components/HeaderSubNav.tsx
+++ components/HeaderSubNav.tsx
@@ -12,13 +12,14 @@ import { useCategories } from "../hooks/useCategories";
 import { IDefaultSearch } from "../app-store/app-defaults/types";
 import { useSelector } from "react-redux";
 import { getDefaultSearch } from "../app-store/session/session.slice";
+import { IProductSubCategory } from "../app-store/types";
 
 export default function HeaderSubNav() {
   const router = useRouter();
   const storeSearch: any = useSelector<IDefaultSearch>(getDefaultSearch);
   const { categories } = useCategories();
 
-  const [subCategories, setSubCategories] = useState([]);
+  const [subCategories, setSubCategories] = useState<IProductSubCategory[]>();
 
   const onCategorySelect = (key, querySlug) => {
     const { location } = storeSearch;
@@ -31,12 +32,14 @@ export default function HeaderSubNav() {
   };
 
   useEffect(() => {
+    const hiddenCategories: number[] = [59, 60, 62, 48, 32, 50, 30];
+
     if (categories) {
-      const subCategories: any = categories[0]?.subCategories?.map((sc) => ({
-        label: sc.title,
-        key: sc.id,
-        slug: sc.slug,
-      }));
+      const subCategories: IProductSubCategory[] = categories[0].subCategories ? categories[0]?.subCategories
+        ?.filter((sc: IProductSubCategory) => {
+          return sc.id && !hiddenCategories.includes(sc.id);
+        }) : [];
+
       setSubCategories(subCategories);
     }
   }, [storeSearch, categories]);
@@ -48,20 +51,21 @@ export default function HeaderSubNav() {
           <div className="relative justify-center w-full inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <div className=" inset-y-0 left-0 items-center sm:gap-x-6 flex overscroll-contain w-full overflow-x-auto relative">
               {subCategories &&
-                subCategories.map((cat: any) => {
-                  return (
-                    <Disclosure.Button
-                      key={cat.key}
-                      onClick={() => onCategorySelect(cat.key, cat.slug)}
-                      className={
-                        "whitespace-nowrap text-sm inline-flex items-center justify-center rounded-md p-2 text-gray-100 hover:bg-gray-700 hover:text-white focus:outline-none"
-                      }
-                    >
-                      <span className="sr-only">Open Menu</span>
-                      {cat.label}
-                    </Disclosure.Button>
-                  );
-                })}
+                subCategories
+                  .map((cat: IProductSubCategory) => {
+                    return (
+                      <Disclosure.Button
+                        key={cat.id}
+                        onClick={() => onCategorySelect(cat.id, cat.slug)}
+                        className={
+                          "whitespace-nowrap text-sm inline-flex items-center justify-center rounded-md p-2 text-gray-100 hover:bg-gray-700 hover:text-white focus:outline-none"
+                        }
+                      >
+                        <span className="sr-only">Open Menu</span>
+                        {cat.title}
+                      </Disclosure.Button>
+                    );
+                  })}
             </div>
           </div>
         </div>
