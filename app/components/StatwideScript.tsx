380b2a0b14c87485824e20d83ed67a5dc9906101
Shri Shinde <shrikkant@gmail.com>
2024-09-15 23:16:06 +0530

    do tracking only for users.

--- app/components/StatwideScript.tsx
+++ app/components/StatwideScript.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { selectAuthState } from '../../app-store/auth/auth.slice'
+import { IUser } from '../../app-store/types'
 
 declare global {
   interface Window {
@@ -12,29 +13,35 @@ declare global {
 }
 export const StatwideScript: React.FC = () => {
   const loggedUser = useSelector(selectAuthState)
+  const isAdmin = (user: IUser) => {
+    return user?.role === 'A';
+  }
 
   useEffect(() => {
     if (!loggedUser?.id || !window.featurics) {
       return;
     }
 
-    window.featurics &&
-      window.featurics.init({
-        visitor: {
-          appVisitorId: loggedUser?.id,
-          email: loggedUser?.email_address,
-          firstName: loggedUser?.firstname || 'User ' + loggedUser?.id,
-          lastName: loggedUser?.lastname || '',
-          // You can include additional visitor level key-values here,
-          // as long as it's not one of the above reserved names.
-          visitorProperties: [
-            {
-              key: 'OrganizationId',
-              value: loggedUser?.id,
-            },
-          ],
-        },
-      })
+
+    if (!isAdmin(loggedUser)) {
+      window.featurics &&
+        window.featurics.init({
+          visitor: {
+            appVisitorId: loggedUser?.id,
+            email: loggedUser?.email_address,
+            firstName: loggedUser?.firstname || 'User ' + loggedUser?.id,
+            lastName: loggedUser?.lastname || '',
+            // You can include additional visitor level key-values here,
+            // as long as it's not one of the above reserved names.
+            visitorProperties: [
+              {
+                key: 'OrganizationId',
+                value: loggedUser?.id,
+              },
+            ],
+          },
+        })
+    }
   }, [loggedUser, window.featurics])
 
   return (<>
