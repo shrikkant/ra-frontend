7b8eec8bbe4e8873d05e9c3853305803e4b79f38
Shri Shinde <shrikkant@gmail.com>
2024-09-30 19:54:10 +0530

    remove crap

--- api/admin/orders.api.ts
+++ api/admin/orders.api.ts
@@ -1,54 +1,33 @@
-import { IOrder, IUser } from '../../app-store/types';
+import { IOrder } from '../../app-store/types';
 import httpClient from './../axios.config';
 
 export async function fetchOrders(status: number): Promise<IOrder[]> {
     const orderStatus = status;
-    try {
-        const orders: IOrder[] = await httpClient.get(`/admin/orders?status=${orderStatus}&offset=0`);
-        return orders;
 
-    } catch (e) {
-        throw e;
-    }
+    const orders: IOrder[] = await httpClient.get(`/admin/orders?status=${orderStatus}&offset=0`);
+    return orders;
+
+
 }
 
 export async function fetchOrder(id: number): Promise<IOrder> {
-    try {
-        const order: IOrder = await httpClient.get(`/admin/orders/${id}`);
-        return order;
-    } catch (e) {
-        throw e;
-    }
+    const order: IOrder = await httpClient.get(`/admin/orders/${id}`);
+    return order;
 }
 
 export async function updateStage(id: number, stageInfo): Promise<IOrder> {
-    console.log("Order Id : ", id, " Stage Info : ", stageInfo);
-    try {
-        const response: IOrder = await httpClient.put(`/admin/orders/${id}`, stageInfo);
-        return response && response[0];
-    } catch (e) {
-        throw e;
-    }
+    const response: IOrder = await httpClient.put(`/admin/orders/${id}`, stageInfo);
+    return response && response[0];
 }
 
 export async function applyDiscount(id: number, transactionId: number, discountInfo): Promise<IOrder> {
-    console.log("Order Id : ", id, " Transaction Id : ", transactionId, " Discount Info : ", discountInfo);
-    try {
-        const response: IOrder = await httpClient.put(`/admin/orders/${id}/transactions/${transactionId}/applyDiscount`, discountInfo);
-
-        return response && response
-    } catch (e) {
-        throw e;
-    }
+    const response: IOrder = await httpClient.put(`/admin/orders/${id}/transactions/${transactionId}/applyDiscount`, discountInfo);
+    return response
 }
 
 export async function assignDeliveryRep(deliveryAssignment): Promise<IOrder> {
-    try {
-        const response: IOrder = await httpClient.post(`/admin/delivery/`, deliveryAssignment);
-        return response && response
-    } catch (e) {
-        throw e;
-    }
+    const response: IOrder = await httpClient.post(`/admin/delivery/`, deliveryAssignment);
+    return response
 }
 
 
