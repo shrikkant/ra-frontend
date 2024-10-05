d0d02a91b540c83b146da2353a71ca5e99c2bf34
Shri Shinde <shrikkant@gmail.com>
2024-10-05 10:48:05 +0530

    fix ux

--- components/MyPageHeader.tsx
+++ components/MyPageHeader.tsx
@@ -4,14 +4,16 @@ import { ArrowLeftIcon, } from "@heroicons/react/24/outline";
 
 export default function MyPageHeader({ ...props }) {
 
-  return (<div className={"flex p-4 gap-2"}>
-    <div className="flex-20" onClick={() => window.history.back()}>
-      <ArrowLeftIcon className="h-6 w-6" />
+  return (<div className={"flex p-4 gap-2 justify-between items-center "}>
+    <div onClick={() => window.history.back()} className="flex mt-4 gap-x-2">
+      <span>
+        <ArrowLeftIcon className="h-6 w-6" />
+      </span>
+      <span>
+        {props.title}
+      </span>
     </div>
-    <div className="text-bold flex-1">
-      {props.title}
-    </div>
-    <div className="flex-1 flex justify-end">
+    <div className="flex justify-end">
       {props.children}
     </div>
 
