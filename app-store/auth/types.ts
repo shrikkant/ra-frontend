104891a8ea0208595920d6cc3f699f0b98d791a5
Shri Shinde <shrikkant@gmail.com>
2024-10-09 20:58:36 +0530

    adding aadhaar type

--- app-store/auth/types.ts
+++ app-store/auth/types.ts
@@ -5,3 +5,26 @@ export interface IAuthState {
 	user?: IUser;
 	isAdminLogin?: boolean;
 }
+
+export interface IAadhaar {
+	full_name: string;
+	aadhaar_number: string;
+	dob: Date;
+	gender: string;
+	address: {
+		country: string;
+		dist: string;
+		state: string;
+		po: string;
+		loc: string;
+		vtc: string;
+		subdist: string;
+		street: string;
+		house: string;
+		landmark: string;
+	},
+	face_status: boolean,
+	face_score: -1,
+	zip: string;
+	profile_image: string;
+}
