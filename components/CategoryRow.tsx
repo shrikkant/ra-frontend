8d60ae9905e4f467fc2fa8f3433e5dcadbbf529e
Shri Shinde <shrikkant@gmail.com>
2024-10-11 11:43:04 +0530

    fix

--- components/CategoryRow.tsx
+++ components/CategoryRow.tsx
@@ -2,13 +2,14 @@
 import { Content } from "antd/lib/layout/layout";
 import ProductCard from "./ProductCard";
 import React from "react";
+import { IProduct } from "../app-store/types";
 
 export default function CategoryRow(props: any) {
   return (
-      <Content className="r-comp flex gap-1 sm:gap-4 flex-wrap p-2">
-        {props.category.products.map((product: any) => (
-          <ProductCard key={product.id} product={product} priority={true}></ProductCard>
-        ))}
-      </Content>
-   );
+    <Content className="r-comp flex gap-1 sm:gap-4 flex-wrap p-2">
+      {props.category.products.map((product: IProduct) => (
+        <ProductCard key={product.id} product={product}></ProductCard>
+      ))}
+    </Content>
+  );
 }
