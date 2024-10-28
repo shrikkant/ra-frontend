dcb095e22c0ba41b1121e022374f985ef17a7a57
Shri Shinde <shrikkant@gmail.com>
2024-10-28 09:08:53 +0530

    fix

--- components/common/Modal.tsx
+++ components/common/Modal.tsx
@@ -32,7 +32,7 @@ const Modal: React.FC<ModalProps> = ({ show, onClose, title, children }) => {
 
       <div className="modal-overlay fixed  bg-opacity-75 bg-gray-500 h-screen w-screen left-0 top-0 z-10" aria-hidden="true"></div>
       <div className=" fixed inset-0 w-screen overflow-y-auto h-screen left-0 top-0 z-10">
-        <div className="modal-content flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0 z-10">
+        <div className="modal-content flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0 z-10">
           <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm xs:w-full z-10">
             <div className="border border-[#FFDC2DAD]">
               <div>
