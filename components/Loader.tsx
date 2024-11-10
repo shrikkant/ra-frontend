7a108e93461b099dd062bee3f338127396d01a97
Shri Shinde <shrikkant@gmail.com>
2024-11-10 09:54:53 +0530

    try to center it

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,7 +3,7 @@ import { Content } from "antd/lib/layout/layout";
 import React from "react";
 export default function Loader() {
   return (
-    <Content style={{ height: "100vh" }}>
+    <Content style={{ height: 'calc(100vh - 140px)' }}>
       <div className="flex justify-center items-center h-full">
         <div
           className="animate-spin inline-block w-8 h-8 border-[3px] border-current border-t-transparent text-blue-600 rounded-full dark:text-blue-600 "
