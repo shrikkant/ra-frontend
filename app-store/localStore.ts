798bfed12a83f70958c04919ee52ad68f985244a
Shri Shinde <shrikkant@gmail.com>
2024-12-08 15:34:55 +0530

    fix

--- app-store/localStore.ts
+++ app-store/localStore.ts
@@ -1,8 +1,11 @@
 export const loadState = <T>(key: string): T | undefined => {
   try {
-    const serializedState = localStorage.getItem(key);
-    if (serializedState === null) return undefined; // No state saved
-    return JSON.parse(serializedState) as T;
+    if (typeof window !== 'undefined') {
+
+      const serializedState = localStorage.getItem(key);
+      if (serializedState === null) return undefined; // No state saved
+      return JSON.parse(serializedState) as T;
+    }
   } catch (err) {
     console.error('Failed to load state:', err);
     return undefined;
@@ -12,8 +15,10 @@ export const loadState = <T>(key: string): T | undefined => {
 
 export const saveState = <T>(key: string, state: T): void => {
   try {
-    const serializedState = JSON.stringify(state);
-    localStorage.setItem(key, serializedState);
+    if (typeof window !== 'undefined') {
+      const serializedState = JSON.stringify(state);
+      localStorage.setItem(key, serializedState);
+    }
   } catch (err) {
     console.error('Failed to save state:', err);
   }
--- components/common/Scripts.tsx
+++ components/common/Scripts.tsx
@@ -22,6 +22,11 @@ export default function Scripts() {
         return;
       }
     }
+
+
+    document.body.classList.add('animated-page');
+    document.body.classList.add('page-loaded');
+
   }, [lastLink, loggedUser, pathname]);
 
   return (<>
