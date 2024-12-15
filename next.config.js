87f291876b1ae5388d3c870280553c2727ab59b3
Shri Shinde <shrikkant@gmail.com>
2024-12-15 14:32:18 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   output: 'standalone',
   publicRuntimeConfig: {
     BASE_API_URL: process.env.REACT_APP_API_URL,
