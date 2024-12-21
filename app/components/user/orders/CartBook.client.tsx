53fae64a695834b06676a4d47c65053a784e6f88
Shri Shinde <shrikkant@gmail.com>
2024-12-21 12:13:47 +0530

    fix shit

--- app/components/user/orders/CartBook.client.tsx
+++ app/components/user/orders/CartBook.client.tsx
@@ -102,7 +102,7 @@ export default function CartBook() {
     if (loggedUser?.address && loggedUser.address.length > 0) {
       selectAddress(loggedUser.address[0]);
     }
-  }, []);
+  }, [cart, loggedUser]);
 
   return (
 
