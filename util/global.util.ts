1a6d349e1f782765ba035cf7db68948e23ac3d88
Shri Shinde <shrikkant@gmail.com>
2025-03-21 16:04:06 +0530

    fix

--- util/global.util.ts
+++ util/global.util.ts
@@ -31,6 +31,9 @@ export const enum OrderStages {
 
 }
 
+export const capitalize = (str) => {
+	return str.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+}
 export const displayMessage = (type: string, text: string) => {
 	switch (type) {
 		case "success":
