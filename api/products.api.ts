8983f36bf18d986508fa58f3cb5b51cbfeed97ed
Shri Shinde <shrikkant@gmail.com>
2025-05-04 17:24:36 +0530

    fix

--- api/products.api.ts
+++ api/products.api.ts
@@ -2,12 +2,9 @@
 import { IProduct, IProductCategory, IProductFilter, ProductPhoto } from '../app-store/types';
 import httpClient, { fetchData } from './axios.config';
 
-
 export async function getFeaturedProducts(pageLimit: number, city: string): Promise<any[]> {
-
   const response = fetchData(`getFeaturedProducts?pageLimit=${pageLimit}&city=${city}`); //await httpClient.get<IProduct[], IProduct[]>(``);
   return response;
-
 }
 
 export async function fetchProductCategories(): Promise<IProductCategory[]> {
@@ -20,34 +17,31 @@ export async function fetchCategoriesClient(): Promise<IProductCategory[]> {
   return response;
 }
 
-export async function fetchProducts(searchString?: string,
+export async function fetchProducts(
+  searchString?: string,
   filter?: IProductFilter,
-  client?: boolean): Promise<{ results: IProduct[], meta }> {
-
+  client?: boolean
+): Promise<{ results: IProduct[]; meta }> {
   const PAGE_LIMIT = 24;
   const pageNumber = filter ? (filter.page ? filter.page : 0) : 0;
   const pageFilter = `&pageLimit=${PAGE_LIMIT}&pageNumber=${pageNumber}`;
-  const cityFilter = filter?.city ? `&city=${filter?.city}` : "";
-  const stateFilter = filter?.state ? `&state=${filter?.state}` : "";
-  const searchQuery = searchString ? searchString.replace(" ", "+") : "";
+  const cityFilter = filter?.city ? `&city=${filter?.city}` : '';
+  const stateFilter = filter?.state ? `&state=${filter?.state}` : '';
+  const searchQuery = searchString ? searchString.replace(' ', '+') : '';
 
-  const rateFilter = filter?.rate ? "&rate=" + filter.rate[0] + "-" + filter.rate[1] : "";
-  const brandFilter = filter?.brand ? "&brands=" + filter.brand.map(b => b + " ") : "";
-  const catFilter = filter?.subCategory ? "&subCat=" + filter?.subCategory : "";
+  const rateFilter = filter?.rate ? '&rate=' + filter.rate[0] + '-' + filter.rate[1] : '';
+  const brandFilter = filter?.brand ? '&brands=' + filter.brand.map(b => b + ' ') : '';
+  const catFilter = filter?.subCategory ? '&subCat=' + filter?.subCategory : '';
 
-
-  const url =
-    `products/?searchString=${searchQuery + pageFilter + stateFilter + cityFilter + rateFilter + brandFilter + catFilter}`;
+  const url = `products/?searchString=${searchQuery + pageFilter + stateFilter + cityFilter + rateFilter + brandFilter + catFilter}`;
   if (client) {
     return httpClient.get(url);
   } else {
     return fetchData(url);
   }
-
 }
 
 export async function fetchProductBySlug(slug: string): Promise<IProduct> {
-
   const response = await fetchData(`products/.by.slug/${slug}`);
   return response;
 }
@@ -55,10 +49,11 @@ export async function fetchProductBySlug(slug: string): Promise<IProduct> {
 export const fetchProductPhoto = async (id: number): Promise<ProductPhoto> => {
   const response: any = await httpClient.get<ProductPhoto>(`products/${id}/photo`);
   return response;
-}
-
+};
 
 export async function fetchProduct(filter): Promise<IProduct> {
-  const response = await httpClient.get<string, IProduct>(`products/.by.slug/${filter.product}?city=${filter?.city}&subCat=${filter.subCategory}`);
+  const response = await httpClient.get<IProduct>(
+    `products/.by.slug/${filter.product}?city=${filter?.city}&subCat=${filter.subCategory}`
+  );
   return response;
 }
