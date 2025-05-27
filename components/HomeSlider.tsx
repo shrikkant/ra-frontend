9f5df7141104e9431dca2101aae6b080a6caec24
Shri Shinde <shrikkant@gmail.com>
2025-05-27 20:07:51 +0530

    Fixed

--- components/HomeSlider.tsx
+++ components/HomeSlider.tsx
@@ -290,7 +290,7 @@ export function HomeSlider() {
         {items && items.map(item => item)}
       </div>
 
-      <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 z-30">
+      <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 z-10">
         <div className="flex items-center gap-2">
           <button
             onClick={prev}
