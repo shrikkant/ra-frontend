7845619bb9fff0f6d4826331fd8589e1f64f2b69
Shri Shinde <shrikkant@gmail.com>
2026-01-07 21:56:37 +0530

    Fix

---
+++ components/seo/JsonLd.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface JsonLdProps {
+  data: object | object[]
+}
+
+/**
+ * Reusable component to render JSON-LD structured data
+ * Supports single schema or array of schemas
+ */
+export const JsonLd: React.FC<JsonLdProps> = ({data}) => {
+  return null
+}
